bbfe42ff4469d66d87a39cdf31eb6d75967a312d
Shatabdi Bhattacharjee <shatabdi17@gmail.com>
2018-06-28 10:22:04 -0400

    made the list collapsible and added styles to the caret button

--- dev/scripts/Listcard.js
+++ dev/scripts/Listcard.js
@@ -8,49 +8,49 @@ constructor (props) {
     super(props)
     this.state = {
         newCollection:listCollection,
-        // show: true
+        show: true
     }
 
     this.removeItem = this.removeItem.bind(this);
-//     this.toggleList = this.toggleList.bind(this);
+    this.toggleList = this.toggleList.bind(this);
  }
 
 removeItem (index) {  
    this.setState({
-       newCollection: this.state.newCollection.filter((item, id) => id != index)
-
+       newCollection: this.state.newCollection.filter((item,id) => id != index)
    })
 }
 
-    // toggleList () {
-    //     const {show} = this.state;
-    //     this.setState({show :!show})
-    // }
+toggleList () {
+const {show} = this.state;
+this.setState({show :!show})
+}
 
-    render() {
-        return (
-                <section className="list-container">
-                <div className="list-container-header">
-                    <p>List Name <span className="list-number">1</span></p>
-                    <p>Total {this.state.newCollection.length}</p>
-                    {/* <button onClick={this.toggleList}>
-                        Click to toggle
-                    </button> */}
-                </div>
-                {/* {this.state.show && <Listitem/>} */}
-                {this.state.newCollection.map((list, id) => {
-                    return <Listitem list={list} key={id} index={id} removeItem={this.removeItem} />
-                    })
-                }     
-          
-                <div className="load-more">
-                    <a href="">Load More</a>
-                </div>
-                </section>
-        )
-    }
+render() {
+    return (
+            <section className="list-container">
+            <div className="list-container-header">
+            <div>
+                <p>List Name <span className="list-number">1</span></p>
+                <p>Total {this.state.newCollection.length}</p>
+            </div>
+                <button className="toggle-button" onClick={this.toggleList}>
+                    <i class="fas fa-caret-down"></i>
+                </button>
+            </div>
+            {this.state.show && 
+            this.state.newCollection.map((list, id) => {
+                return <Listitem list={list} key={id} index={id} removeItem={this.removeItem} />
+                })
+            }    
+            
+            <div className="load-more">
+                <a href="">Load More</a>
+            </div>
+            </section>
+    )
+}
 }
 
 export default Listcard;
 
- 
